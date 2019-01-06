ee2888c757af8f96649790f85007a1ad14cd7a0f
Zhu Zhan Yan <program.nom@gmail.com>
2019-01-06 18:29:43 +0800

    Reimplmented content loss with tensorboard

--- src/stylenet/style.py
+++ src/stylenet/style.py
@@ -7,6 +7,7 @@
 import numpy as np
 import tensorflow as tf
 import keras.backend as K
+import matplotlib.pyplot as plt
 
 from PIL import Image
 from keras.models import Model
@@ -14,12 +15,12 @@ from keras.layers import InputLayer
 from keras.applications.vgg16 import VGG16
 
 # Style transfer settings
-IMAGE_SHAPE = (512, 512, 3)
+IMAGE_SHAPE = (64, 64, 3)
 
 # Loss computation weights
-CONTENT_WEIGHT = 0
-STYLE_WEIGHT = 8e+2
-TOTAL_VARIATION_WEIGHT = 4
+CONTENT_WEIGHT = 0.025
+STYLE_WEIGHT = 5.0
+DENOISE_WEIGHT = 1.0
 
 # Layers for feature extraction
 CONTENT_LAYERS = ['block2_conv2']
@@ -89,113 +90,123 @@ def deprocess_image(img_mat):
 # Build and returns a model that returns features tensors given an input tensor
 # that extracts features based on the given layers names
 def build_extractor(layer_names):
-    # Load VGG model
-    vgg_model = VGG16(input_shape=IMAGE_SHAPE, weights="imagenet", include_top=False)
-    vgg_model.trainable = False
+    with tf.name_scope("feature_extractor"):
+        # Load VGG model
+        vgg_model = VGG16(input_shape=IMAGE_SHAPE, weights="imagenet", include_top=False)
+        vgg_model.trainable = False
 
-    # Build  model by extracting  feature tensors
-    layers = [ vgg_model.get_layer(name) for name in layer_names ]
-    feature_ops = [ layer.output for layer in layers ] 
-    model = Model(inputs=[vgg_model.input], outputs=feature_ops)
+        # Build  model by extracting  feature tensors
+        layers = [ vgg_model.get_layer(name) for name in layer_names ]
+        feature_ops = [ layer.output for layer in layers ] 
+        model = Model(inputs=[vgg_model.input], outputs=feature_ops)
+        
+        # Ensure that model always outputs list
+        def wrapped_model(x):
+            y = model(x)
+            return y if type(y) is list else [y]
 
-    return model
+        return wrapped_model
 
 # Build and return the gram matrix tensor for the given input features tensor
 def build_gram_matrix(input_op):
-    # Flatten input tensor, leaving features dimention
-    n_features = input_op.shape[-1].value
-    input_op = K.reshape(input_op, (-1, n_features))
-    
-    # Compute gram matrix
-    gram_mat_op = K.dot(K.transpose(input_op), input_op)
-    return gram_mat_op
+    with tf.name_scope("gram_matrix"):
+        # Flatten input tensor, leaving features dimention
+        n_features = input_op.shape[-1].value
+        input_op = K.reshape(input_op, (-1, n_features))
+        
+        # Compute gram matrix
+        gram_mat_op = K.dot(K.transpose(input_op), input_op)
+        return gram_mat_op
 
 ## Loss functions
 # Build and return tensor that computes content loss given the 
 # pastiche and content image tensors
 def build_content_loss(pastiche_op, content_op):
-    # Shape tensors for feature extraction
-    pastiche_op = tf.expand_dims(pastiche_op, axis=0)
-    content_op = tf.expand_dims(content_op, axis=0)
-    
-    # Extract content features using content extractor
-    extractor = build_extractor(CONTENT_LAYERS)
-    pastiche_feature_op = extractor(pastiche_op)
-    content_feature_op = extractor(content_op)
-    
-    # Compute content loss
-    loss = tf.losses.mean_squared_error(pastiche_feature_op, content_feature_op)
+    with tf.name_scope("content_loss"):
+        # Shape tensors for feature extraction
+        pastiche_op = tf.expand_dims(pastiche_op, axis=0)
+        content_op = tf.expand_dims(content_op, axis=0)
+        
+        # Extract content features using content extractor
+        extractor = build_extractor(CONTENT_LAYERS)
+        pastiche_feature_ops = extractor(pastiche_op)
+        content_feature_ops = extractor(content_op)
+        
+        # Compute content loss
+        differences = [ P - C for P, C in zip(pastiche_feature_ops, 
+                                              content_feature_ops) ]
+                       
+        loss = tf.reduce_sum(tf.square(differences), name="content_loss")
 
-    return loss
+        return loss
 
-# Build and return tensor that computes style loss given the 
-# pastiche and style image tensors
-def build_style_loss(pastiche_op, style_op):
-    # Shape tensors for feature extraction
-    pastiche_op = tf.expand_dims(pastiche_op, axis=0)
-    style_op = tf.expand_dims(style_op, axis=0)
+# Build and return a tensor that computes total variation loss
+# Loss attempts to reduce the noise in the generated images
+def build_noise_loss(pastiche_op):
+    with tf.name_scope("noise_loss"):
+        #TODO: implement multiple layers
+        # Compute variation accross image axis
+        height_variation = K.sum(K.abs(pastiche_op[:-1, :, :] - pastiche_op[1:, :, :]))
+        width_variation = K.sum(tf.abs(pastiche_op[:, :-1, :] - pastiche_op[:, 1:, :]))
+        
+        total_variation = height_variation + width_variation
 
-    # Extract style features using style extractor
-    extractor = build_extractor(STYLE_LAYERS)
-    pastiche_feature_ops = extractor(pastiche_op)
-    style_feature_ops = extractor(style_op)
-    
-    pastiche_gram_ops = [ build_gram_matrix(f) for f in pastiche_feature_ops ]
-    style_gram_ops = [ build_gram_matrix(f) for f in style_feature_ops ]
-    
-    # Compute style loss
-    losses = []
-    for pastiche_feature_op, style_feature_op in zip(pastiche_feature_ops, 
-                                                     style_feature_ops):
-        # Define constants
-        n_batch, n_height, n_width, n_features = pastiche_feature_op.shape.as_list()
-        N = n_features
-        M = n_height * n_width
-
-        # Compute gram matrix representations of style features
-        pastiche_gram_op = build_gram_matrix(pastiche_feature_op)
-        style_gram_op = build_gram_matrix(style_feature_op)
-    
-        # Compute layer contribution to loss
-        loss = tf.losses.mean_squared_error(pastiche_gram_op, 
-                                            style_gram_op) / (4 * (N ** 2) * (M ** 2))
-        losses.append(loss)
-    
-    return K.sum(losses)
-    
+        return total_variation
 
 # Build and return tensor that total style transfer loss given the
 # pastiche, content and style image tensors 
 def build_loss(pastiche_op, content_op, style_op):
-    content_loss_op = build_content_loss(pastiche_op, content_op)
-    style_loss_op = build_style_loss(pastiche_op, style_op)
+    with tf.name_scope("style_transfer_loss"):
+        content_loss_op = build_content_loss(pastiche_op, content_op)
+        style_loss_op = build_style_loss(pastiche_op, style_op)
+        noise_loss_op = build_noise_loss(pastiche_op)
 
-    # Total loss weight sum of content and style Losses
-    loss = CONTENT_WEIGHT * content_loss_op + STYLE_WEIGHT * style_loss_op
+        # Total loss weight sum of content and style Losses
+        loss = CONTENT_WEIGHT * content_loss_op \
+            + STYLE_WEIGHT * style_loss_op \
+            + DENOISE_WEIGHT * noise_loss_op
 
-    return loss
+        return loss
+    
+def build_summary(loss_op):
+    loss_summary = tf.summary.scalar("Style Transfer Loss", loss_op)
+    summary_op = tf.summary.merge([loss_summary])
+    
+    return summary_op
 
 if __name__ == "__main__":
     content = preprocess_image(Image.open("./data/Tuebingen_Neckarfront.jpg"))
     style = preprocess_image(Image.open("./data/stary_night.jpg"))
-    pastiche = np.random.uniform(size=IMAGE_SHAPE) * 256
+    pastiche = np.random.uniform(size=IMAGE_SHAPE) * 256.0 - 128.0
     
+    K.clear_session()
     session = tf.Session()
     K.set_session(session)
-    pastiche_op = K.variable(pastiche)
-    content_op = K.constant(content)
-    style_op = K.constant(style)
 
-    loss_op = build_loss(pastiche_op, content_op, style_op)
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=3e+1)
-    train_op = optimizer.minimize(loss_op, var_list=[ pastiche_op ])
+    pastiche_op = K.variable(pastiche, name="pastiche")
+    content_op = K.constant(content, name="content")
+    style_op = K.constant(style, name="style")
     
+    loss_op = build_content_loss(pastiche_op, content_op)
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e+2)
+    train_op = optimizer.minimize(loss_op, var_list=[pastiche_op])
+
+    writer = tf.summary.FileWriter("logs", session.graph)
+    summary_op = build_summary(loss_op)
+
     session.run(tf.global_variables_initializer())
-    
-    for i in range(150):
-        _, loss, pastiche = session.run([train_op, loss_op, pastiche_op])
-        print(i," - loss: ", loss)
-    
-        img = deprocess_image(pastiche)
-        img.save("pastiche/{}.jpg".format(i))
+    n_epochs = 100
+    for i in range(n_epochs):
+        feed = {K.learning_phase(): 0}
+        _, loss, pastiche, summary = session.run([train_op, loss_op, pastiche_op,
+                                                  summary_op],
+                                                 feed_dict=feed)
+        print("[{}/{}] loss: {:e}".format(i, n_epochs, loss))
+        
+        pastiche_image = deprocess_image(pastiche)
+        plt.imshow(np.asarray(pastiche_image))
+        plt.draw()
+        plt.pause(1e-9)
+
+        writer.add_summary(summary, i)
