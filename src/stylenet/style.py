0b7a09b37c5ee347f997d924c88397edaad1cbb5
Zhu Zhan Yan <program.nom@gmail.com>
2019-01-08 23:58:56 +0800

    update model params

--- src/stylenet/style.py
+++ src/stylenet/style.py
@@ -20,8 +20,8 @@ IMAGE_SHAPE = (512, 512, 3)
 
 # Loss computation weights
 CONTENT_WEIGHT = 1
-STYLE_WEIGHT = 5
-DENOISE_WEIGHT = 1e-5
+STYLE_WEIGHT = 10
+DENOISE_WEIGHT = 1e-3
 
 # Layers for feature extraction
 CONTENT_LAYERS = ['block5_conv2']
