d72062e105c4425d237c963f7fbf5577108cdf2d
Zhu Zhan Yan <program.nom@gmail.com>
2019-01-28 19:09:45 +0800

    Added abiity read content from file input

---
+++ frontend/assets/js/edit.js
@@ -0,0 +1,29 @@
+/*
+ * assets/js/edit.js
+ * Pasitche Frontend
+ * Client side JS for Edit Page
+*/
+
+
+/* Read the data from the given file selected by the given file input element
+ * Calls the given onread callback when the file is sucessfully read
+ * NOTE: assumes only one file is selected with input
+*/
+function readFileInput(input, onread) {
+    if(input.files.length != 1) throw "No or multiple files selected";
+
+    // Read file with HTML5 file reader
+    const file =  input.files[0];
+    reader = new FileReader();
+    reader.onload = (event) => {
+        // Call callback with file data
+        onread(event.target.result);
+    }
+}
+
+
+$(document).ready(() => {
+    $(".inputfile").on("change", (event) => {
+        readFileInput(event.target, (data) => console.log("got data: ", data));
+    });
+});
