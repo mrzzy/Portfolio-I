df4ad438dd8e3207bbe0fcd480805c9ef7f0f8cc
Zhu Zhan Yan <program.nom@gmail.com>
2019-01-28 22:05:31 +0800

    Added tunnels for style transfer server and tensorboard

--- backend/tunnel.sh
+++ backend/tunnel.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-autossh -M 0 -L 2222:localhost:2222 -N zzy@snitch.ddns.net
+# Cortex Tunnels
+# Ports:
+# =======
+# 2222 - ssh
+# 6006 - Tensorboard
+# 8080 - Frontend
+# 8989 - Backend 
+
+ssh -o ServerAliveInterval=60 \
+-J zzy@snitch.ddns.net \
+-L 2222:localhost:22 \
+-L 6006:localhost:6006 \
+-L 8989:localhost:8989 \
+-N zzy@localhost -p 2222
